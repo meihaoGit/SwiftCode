ef35b40686b41084693b38348f913f6765353037
meihaoGit <meihao15@hotmail.com>
2014-06-16 23:32:14 +0900

    V1.0

---
+++ HelloWord/HelloWord/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HelloWord
+//
+//  Created by  meihao on 2014/06/16.
+//  Copyright (c) 2014年  meihao. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
