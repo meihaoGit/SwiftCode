f78061ea34ff1ed496e3af5e747c2848099719e9
meihaoGit <meihao15@hotmail.com>
2014-06-18 00:15:05 +0900

    OK

--- HelloWord/HelloWord/main.swift
+++ HelloWord/HelloWord/main.swift
@@ -30,6 +30,13 @@ dare1.chinpui()
 println("dare1: \(dare1.name())")
 println("dare2: \(dare2.name())")
 
+
+let people = ["Anna":67,"Beto":8,"Jack":33,"Sam":25]
+
+for (name,age) in people{
+    println("\(name) is \(age) years old .")
+}
+
 //println(ErrorMsg)
 
 
